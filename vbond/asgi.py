72fbde0ad755ca654a8e861d8c6a2122ae13286f
Paravasthu <shashankp96@gmail.com>
2020-11-04 13:17:06 +0530

    changed vbond_portal to vbond;

--- vbond/asgi.py
+++ vbond/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vbond_portal project.
+ASGI config for vbond project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbond_portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbond.settings')
 
 application = get_asgi_application()
